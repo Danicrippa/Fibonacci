060b1b2639fa5558e88b2853eeaab9c0fa8cc4b8
Danicrippa <86597427+Danicrippa@users.noreply.github.com>
2021-07-05 13:55:36 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+
+                static void Main(string[] args)
+        {
+
+            int a = 0, b = 2, c = 4;
+           
+            Console.WriteLine("Quanti valori:");
+           
+            int valori = int.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Sequenze Fibonacci con" + valori + "valori");
+           
+            for (int i = 0; i < valori; i++)
+            {
+                if (i < valori - 1)
+                {
+                 Console.Write(a + ",");
+                }
+                else
+                {
+                 Console.Write(a);
+                }
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
+
+
